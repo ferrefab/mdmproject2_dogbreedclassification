6a44702bb2ed488a94209fd9530935ec9f32528b
Fabio <ferrefab@students.zhaw.ch>
2024-05-18 14:48:31 +0200

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:17-jdk-slim
+FROM openjdk:21-jdk-slim
 
 # Copy Files
 WORKDIR /usr/src/app
@@ -8,5 +8,5 @@ COPY . .
 RUN ./mvnw -Dmaven.test.skip=true package
 
 # Docker Run Command
-EXPOSE 8080
+EXPOSE 8082
 CMD ["java","-jar","/usr/src/app/target/playground-0.0.1-SNAPSHOT.jar"]
--- src/main/java/ch/zhaw/deeplearningjava/footwear/ClassificationController.java
+++ src/main/java/ch/zhaw/deeplearningjava/footwear/ClassificationController.java
@@ -1,11 +1,23 @@
 package ch.zhaw.deeplearningjava.footwear;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+
+import org.springframework.core.io.Resource;
+import org.springframework.beans.propertyeditors.InputStreamEditor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
+
+import java.io.InputStream;
 
 @RestController
 public class ClassificationController {
@@ -19,8 +31,27 @@ public class ClassificationController {
 
     @PostMapping(path = "/analyze")
     public String predict(@RequestParam("image") MultipartFile image) throws Exception {
-        System.out.println(image);
-        return inference.predict(image.getBytes()).toJson();
+        InputStream is = new ByteArrayInputStream(image.getBytes());
+        var uri = "http://localhost:8080/predictions/resnet18_v1";
+        if (this.isDockerized()) {
+            uri = "http://model-service:8080/predictions/resnet18_v1";
+        }
+        var webClient = WebClient.create();
+        Resource resource = new InputStreamResource(is);
+        var result = webClient.post()
+                              .uri(uri)
+                              .contentType(MediaType.MULTIPART_FORM_DATA)
+                              .body(BodyInserters.fromResource(resource))
+                              .retrieve()
+                              .bodyToMono(String.class)
+                              .block();
+        return result;
     }
+    
+    private boolean isDockerized() {
+        File f = new File("/.dockerenv");
+        return f.exists();
+    }
+    
 
 }
